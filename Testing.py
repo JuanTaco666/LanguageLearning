9ca5c66246f03b5153fa5d3771f8e1ed5e688ce6
JuanTaco666 <agankin02@gmail.com>
2022-04-09 15:48:45 -0400

    Made TSV filenames better and made testing class

---
+++ Testing.py
@@ -0,0 +1,13 @@
+import csv
+with open("EnglishWords.tsv") as file:
+       
+    # Passing the TSV file to 
+    # reader() function
+    # with tab delimiter
+    # This function will
+    # read data from file
+    tsv_file = csv.reader(file, delimiter="\t")
+     
+    # printing data line by line
+    for line in tsv_file:
+        print(line)
